cfe6e4a8f1918f88e2bca8cb056cc0ed00929f31
FluffySealy <38732129+FluffySealy@users.noreply.github.com>
2020-10-02 22:16:43 +0100

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+var rellax = new Rellax('.rellax', {
+    center: true,
+    speed: 2
+  });
+  setTimeout(function(){ rellax.refresh(); }, 2000);
+  setTimeout(function(){ rellax.refresh(); }, 5000);
